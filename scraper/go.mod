22e88a710eb66b6ac1ef4c4583db6750e988c3e7
Gabriel Thomé <gabrielallan.thome@gmail.com>
2024-09-30 11:34:43 -0300

    feat(scraper): initial scraping method

---
+++ scraper/go.mod
@@ -0,0 +1,49 @@
+module github.com/gabrieleiro/olx-bets/scraper
+
+go 1.22.4
+
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/joho/godotenv v1.5.1
+	github.com/playwright-community/playwright-go v0.4702.0
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d
+	modernc.org/sqlite v1.33.1
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
+	modernc.org/libc v1.55.3 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.8.0 // indirect
+	modernc.org/strutil v1.2.0 // indirect
+	modernc.org/token v1.1.0 // indirect
+)
---
+++ scraper/main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly/v2"
+	"github.com/joho/godotenv"
+	"github.com/playwright-community/playwright-go"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+	_ "modernc.org/sqlite"
+)
+
+var db *sql.DB
+
+type OLXAd struct {
+	Title string `json:"title"`
+	Image string `json:"image"`
+	Price string `json:"price"`
+	Location string `json:"location"`
+}
+
+func randomPage() ([]OLXAd, error) {
+	var ads []OLXAd
+
+	urls := []string{
+		"https://www.olx.com.br/eletronicos-e-celulares",
+		"https://www.olx.com.br/para-a-sua-casa",
+		"https://www.olx.com.br/autos-e-pecas",
+		"https://www.olx.com.br/imoveis",
+		"https://www.olx.com.br/eletro",
+		"https://www.olx.com.br/moveis",
+		"https://www.olx.com.br/esportes-e-lazer",
+		"https://www.olx.com.br/musica-e-hobbies",
+		"https://www.olx.com.br/agro-e-industria",
+		"https://www.olx.com.br/moda-e-beleza",
+		"https://www.olx.com.br/artigos-infantis",
+		"https://www.olx.com.br/animais-de-estimacao",
+		"https://www.olx.com.br/cameras-e-drones",
+		"https://www.olx.com.br/games",
+		"https://www.olx.com.br/escritorio",
+	}
+
+	url := urls[rand.Intn(len(urls))]
+	now := time.Now()
+
+	filename := fmt.Sprintf("/tmp/olx-%s.html", now)
+
+	ua := "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0"
+
+	pw, err := playwright.Run()
+	if err != nil {
+		log.Printf("could not run playwright\n")
+		return ads, err
+	}
+
+	browser, err := pw.Chromium.Launch()
+	if err != nil {
+		log.Printf("could not launch chromium\n")
+		return ads, err
+	}
+
+	ctx, err := browser.NewContext(playwright.BrowserNewContextOptions{UserAgent: &ua})
+	if err != nil {
+		log.Printf("could not create browser context\n")
+		return ads, err
+	}
+
+ 	page, err := ctx.NewPage()
+  	if err != nil {
+   		log.Printf("could not create new browser tab\n")
+     	return ads, err
+   	}
+
+	if _, err := page.Goto(url); err != nil {
+		log.Printf("could not go to %s\n", url)
+		return ads, err
+	}
+
+	html, err := page.Content()
+	if err != nil {
+		log.Printf("could not read content from page %s", url)
+		return ads, err
+	}
+
+	err = os.WriteFile(filename, []byte(html), 0644)
+	if err != nil {
+		log.Printf("could not write file\n")
+		return ads, err
+	}
+
+	if err = browser.Close(); err != nil {
+		log.Printf("could not close browser\n")
+		return ads, err
+	}
+	if err = pw.Stop(); err != nil {
+		log.Printf("could not stop Playwright\n")
+		return ads, err
+	}
+
+	t := &http.Transport{}
+	t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
+
+	c := colly.NewCollector()
+	c.WithTransport(t)
+
+	c.OnHTML(".olx-ad-card", func(e *colly.HTMLElement) {
+		ads = append(ads, OLXAd{
+			Title: e.ChildText(".olx-ad-card__title"),
+			Price: e.ChildText(".olx-ad-card__price"),
+			Location: e.ChildTexts(".olx-ad-card__location-date-container>p")[0],
+			Image: e.ChildAttr(`source[type="image/jpeg"]`, "srcset"),
+		})
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("Request URL: %s failed with response %v\nError: %v", r.Request.URL, r, err)
+	})
+
+	c.Visit("file://" + filename)
+	c.Wait()
+
+	return ads, err
+}
+
+func main() {
+	var err error
+	if os.Getenv("ENV") != "production" {
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatalf("could not load env file")
+		}
+	}
+
+	db, err = sql.Open("libsql", os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatalf("failed to open db: %s", err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	var interval int
+	interval, _ = strconv.Atoi(os.Getenv("INTERVAL"))
+
+	if interval < 10 && os.Getenv("ENV") != "development" {
+		log.Fatalf("Minimum interval is 10 minutes. Tried to run scraper with interval of %d minutes", interval)
+	}
+
+	go func() {
+		ticker := time.NewTicker(time.Duration(interval) * time.Minute)
+
+		for range ticker.C {
+			log.Printf("fetching olx page\n")
+
+            ads, err := randomPage()
+			if err != nil {
+				log.Printf("could not fetch ads: %v\n", err)
+				continue
+			}
+
+			values := []any{}
+			insert := "INSERT INTO olx_ads (title, price, image, location) VALUES "
+
+			for _, ad := range ads {
+				insert += "(?, ?, ?, ?),"
+				values = append(values, ad.Title, ad.Price, ad.Image, ad.Location)
+			}
+
+			insert = strings.TrimRight(insert, ",")
+
+			_, err = db.Exec(insert, values...)
+
+			if err != nil {
+				log.Printf("could not insert ads in database. ads: %v\nerror: %v\n", ads, err)
+				continue
+			}
+
+			log.Printf("Successfully fetched ads.")
+		}
+	 }()
+
+	select{}
+}
